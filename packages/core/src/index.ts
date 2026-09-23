76c4888320968d6f2cf7e64837abc911a2008c4e
Ahmed Saeed Turki <ahmedsaeedturki@gmail.com>
2026-09-24 02:10:43 +0300

    chore(core): export licensing API

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export * from "./audit/index.js";
 export * from "./campaigns/validation.js";
 export * from "./connectors/index.js";
 export * from "./database/index.js";
+export * from "./licensing/index.js";
 export * from "./queue/index.js";
 export * from "./security/index.js";
 export * from "./types/index.js";
