5b53f71674212ac1841b11ef4727fa8fda9db6aa
Ahmed Saeed Turki <ahmedsaeedturki@gmail.com>
2026-09-24 19:47:56 +0300

    feat(web): link refunds policy from public navigation

--- packages/web/src/app/page.tsx
+++ packages/web/src/app/page.tsx
@@ -19,6 +19,7 @@ export default function HomePage(): JSX.Element {
             <Link href="/pricing/">الأسعار</Link>
             <Link href="/legal/privacy/">الخصوصية</Link>
             <Link href="/legal/terms/">الشروط</Link>
+            <Link href="/legal/refunds/">الاسترداد</Link>
           </nav>
         </div>
       </header>
